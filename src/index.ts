fdb89707279e3b99c6d590cc3e8e8342a762b15f
ndegeulaya1 <ndegeulayabaton@gmail.com>
2025-09-13 02:27:36 +0300

    project set up

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,21 @@
 import { fromHono } from "chanfana";
 import { Hono } from "hono";
-import { TaskCreate } from "./endpoints/taskCreate";
-import { TaskDelete } from "./endpoints/taskDelete";
-import { TaskFetch } from "./endpoints/taskFetch";
-import { TaskList } from "./endpoints/taskList";
 
-// Start a Hono app
+// Import endpoints
+
+
+// Start Hono app
 const app = new Hono<{ Bindings: Env }>();
 
 // Setup OpenAPI registry
 const openapi = fromHono(app, {
-	docs_url: "/",
+  docs_url: "/", // OpenAPI docs at "/"
 });
 
-// Register OpenAPI endpoints
-openapi.get("/api/tasks", TaskList);
-openapi.post("/api/tasks", TaskCreate);
-openapi.get("/api/tasks/:taskSlug", TaskFetch);
-openapi.delete("/api/tasks/:taskSlug", TaskDelete);
 
-// You may also register routes for non OpenAPI directly on Hono
-// app.get('/test', (c) => c.text('Hono!'))
 
-// Export the Hono app
+// You may also register routes directly on Hono
+app.get("/health", (c) => c.text("OK"));
+
+// Export app
 export default app;
